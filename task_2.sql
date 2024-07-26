f117ee90c06d1b69c32f1b0ca61a6fc628e6c49d
Ugochukwu <emdeefx@gmail.com>
2024-07-27 00:57:29 +0100

    Create task_2.sql create all the neccesary tables for alx_book_store

--- task_2.sql
+++ task_2.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id PRIMARY KEY,
     title VARCHAR(130),
     author_id,
@@ -6,30 +6,31 @@ CREATE TABLE IF NOT EXISTS books (
     publication_date DATE
 );
 
-CREATE TABLE IF NOT EXISTS authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id PRIMARY KEY,
     author_name VARCHAR(215),
     FOREIGN KEY (author_id) REFERENCES Books(author_id)
 ):
 
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT,
 );
 
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE IF NOT EXISTS order_details(
+CREATE TABLE IF NOT EXISTS Order_Details(
     order_detail PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
+)
