fc4069cb56091b594085fbaed03abe7ba976ea3b
Ugochukwu <emdeefx@gmail.com>
2024-07-27 00:47:34 +0100

    Changes in line 12, CREATE DATABASE

--- MySQLServer.py
+++ MySQLServer.py
@@ -9,7 +9,7 @@ try:
 
     #create an instance of the connect class 'cursor' to communicate with the Mysql db
     cursor = connection.cursor()
-    value = cursor.execute("CREATE IF NOT EXISTS alx_book_store")
+    value = cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
     connection.commit()
     print("Database 'alx_book_store' created successfully!")
 except Exception as e:
