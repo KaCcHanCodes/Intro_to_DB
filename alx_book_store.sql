9c869cc2c56710d93909fafcd224b4036df5c878
Ugochukwu <emdeefx@gmail.com>
2024-07-27 00:04:32 +0100

    Make changes to each CREATE query to implement IF clause

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,7 +1,7 @@
-CREATE DATABASES alx_book_store;
+CREATE DATABASES IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE TABLE Books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id PRIMARY KEY,
     title VARCHAR(130),
     author_id,
@@ -9,27 +9,27 @@ CREATE TABLE Books (
     publication_date DATE
 );
 
-CREATE TABLE Authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id PRIMARY KEY
     author_name VARCHAR(215)
     FOREIGN KEY(author_id) REFERENCES Books(author_id)
 ):
 
-CREATE TABLE Customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id PRIMARY KEY
     customer_name VARCHAR(215)
     email VARCHAR(215)
     address_ TEXT
 );
 
-CREATE TABLE Orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id (Primary Key)
     customer_id
     order_date DATE
     FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE Order_Details(
+CREATE TABLE IF NOT EXISTS Order_Details(
     order_detail PRIMARY KEY
     order_id
     book_id
