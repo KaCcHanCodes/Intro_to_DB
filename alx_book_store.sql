1ee35a765925aca3c7d4cf673d0ba2660c4226e6
Ugochukwu <emdeefx@gmail.com>
2024-07-27 00:10:56 +0100

    Correct typo in multiple lines and added commas

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -10,30 +10,30 @@ CREATE TABLE IF NOT EXISTS Books (
 );
 
 CREATE TABLE IF NOT EXISTS Authors (
-    author_id PRIMARY KEY
-    author_name VARCHAR(215)
+    author_id PRIMARY KEY,
+    author_name VARCHAR(215),
     FOREIGN KEY(author_id) REFERENCES Books(author_id)
 ):
 
 CREATE TABLE IF NOT EXISTS Customers (
-    customer_id PRIMARY KEY
-    customer_name VARCHAR(215)
-    email VARCHAR(215)
-    address TEXT
+    customer_id PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT,
 );
 
 CREATE TABLE IF NOT EXISTS Orders (
-    order_id (Primary Key)
-    customer_id
-    order_date DATE
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
     FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details(
-    order_detail PRIMARY KEY
-    order_id
-    book_id
-    quantity DOUBLE
-    FOREIGN KEY(order_id) REFERENCES Orders(order_id)
+    order_detail PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
     FOREIGN KEY(book_id) REFERENCES Books(book_id)
 );
